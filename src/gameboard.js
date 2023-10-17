078758047515af2344cc9a3b640a42a3eff9c6dc
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-10-17 18:24:15 +0200

    Add x, y coordinates attributes to chess cell

--- src/gameboard.js
+++ src/gameboard.js
@@ -15,27 +15,45 @@ const addChessboard = () => {
     isLastLine = false,
   ) {
     // put the line numbers of the chessboard from the y-axis to the left
+    // and the column numbers of the chessboard from the x-axis to the bottom
     const startingCellColor = firstCellColor.cloneNode(true);
     startingCellColor.appendChild(lineNumberCell);
+
+    startingCellColor.setAttribute('data-y', lineNumberCell.textContent);
+    firstCellColor.setAttribute('data-y', lineNumberCell.textContent);
+    startingCellColor.setAttribute('data-x', 0);
+
     if (isLastLine) addColumnNumber(startingCellColor, lineNumberCell.cloneNode(true), 0);
     gameboardContainer.appendChild(startingCellColor.cloneNode(true));
+
+    lastCellColor.setAttribute('data-y', lineNumberCell.textContent);
+    lastCellColor.setAttribute('data-x', 1);
+
     if (isLastLine) addColumnNumber(lastCellColor, lineNumberCell, 1);
     gameboardContainer.appendChild(lastCellColor.cloneNode(true));
 
     for (let i = 0; i < 3; i += 1) {
       if (isLastLine) addColumnNumber(firstCellColor, lineNumberCell, i * 2 + 2);
+
+      firstCellColor.setAttribute('data-x', i * 2 + 2);
       gameboardContainer.appendChild(firstCellColor.cloneNode(true));
+
       if (isLastLine) addColumnNumber(lastCellColor, lineNumberCell, i * 2 + 3);
+
+      lastCellColor.setAttribute('data-x', i * 2 + 3);
       gameboardContainer.appendChild(lastCellColor.cloneNode(true));
     }
   }
 
   function addChessCells() {
     const gameboardContainer = document.querySelector('.gameboard-container');
+
     const blackChessCell = document.createElement('div');
     blackChessCell.classList.add('chess-cell', 'black');
-    const whiteChessCell = document.createElement('chess-cell', 'white');
+
+    const whiteChessCell = document.createElement('div', 'white');
     whiteChessCell.classList.add('chess-cell', 'white');
+
     for (let i = 4; i > 0; i -= 1) {
       const lineNumberCell = document.createElement('div');
       lineNumberCell.classList.add('board-number', 'line');
